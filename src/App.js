434815eda31551673f19a6506322fbe1c0b9e35a
zzanguna <yajung0203@naver.com>
2021-02-24 16:38:13 +0900

    router-tutorial - history - withRouter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link, Route } from "react-router-dom";
 import Home from "./component/Home";
 import About from "./component/About";
+import Profiles from "./component/Profiles";
+import HistorySample from "./component/HistorySample";
 
 const App = () => {
   return (
@@ -13,10 +15,18 @@ const App = () => {
         <li>
           <Link to={"/about"}>소개</Link>
         </li>
+        <li>
+          <Link to={"/profiles"}> 프로필</Link>
+        </li>
+        <li>
+          <Link to={"/history"}> History 예제</Link>
+        </li>
       </ul>
       <hr />
       <Route path={"/"} component={Home} exact={true} />
-      <Route path={"/about"} component={About} />
+      <Route path={["/about", "/info"]} component={About} />
+      <Route path={"/profiles"} component={Profiles} />
+      <Route path={"/history"} component={HistorySample} />
     </div>
   );
 };
--- src/component/About.js
+++ src/component/About.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
+import qs from "qs";
 
-const About = () => {
-    return (
-        <div>
-            <h1>소개</h1>
-            <p>이 프로젝트는 리액트 라우터 기초를 실습해 조는 예제 프로젝트 입니다.</p>
-        </div>
-    );
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true, // 이 설정을 통해 문자열 맨 앞의 ?를 생략
+  });
+  const showDetail = query.detail === "true";
+
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>
+        이 프로젝트는 리액트 라우터 기초를 실습해 조는 예제 프로젝트 입니다.
+      </p>
+      {showDetail && <p>detail 값을 true 로 설정하였군요.</p>}
+    </div>
+  );
 };
 
 export default About;
